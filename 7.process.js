3b450d3d76d9492a4b4ac6bda645dc5a7699e1a5
marcochavezco <machavezco@gmail.com>
2024-06-05 23:24:17 -0600

    process

---
+++ 7.process.js
@@ -0,0 +1,11 @@
+// arguments
+console.log(process.argv);
+
+// process.exit(0);
+// process.exit(1);
+
+// process.on('exit', () => {
+//   console.log('Process will exit now');
+// });
+
+console.log(process.cwd());
