7953329bd5ad8e1bafcaa621c8aa2c1a40ead10d
marcochavezco <machavezco@gmail.com>
2024-06-05 22:34:06 -0600

    fs read file sync, async, async parallel

---
+++ 5.fs-async-await.js
@@ -0,0 +1,14 @@
+const fs = require('node:fs/promises');
+(async () => {
+  console.log('Reading files...');
+
+  console.log('Reading file 1...');
+  const text = await fs.readFile('./file.txt', 'utf-8');
+  console.log('File 1:', text);
+
+  console.log('Doing some stuff...');
+
+  console.log('Reading file 2...');
+  const text2 = await fs.readFile('./file.txt', 'utf-8');
+  console.log('File 2:', text2);
+})();
