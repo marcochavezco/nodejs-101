cd2293664cf03830409e391503b7bbb5ae4424a9
marcochavezco <machavezco@gmail.com>
2024-06-06 21:57:38 -0600

    add findAvailablePort function

--- 9.http.js
+++ 9.http.js
@@ -1,10 +1,15 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./10.find-port')
+
+const port = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
   console.log('request event')
   res.end('Hi mom')
 })
 
-server.listen(0, () => {
-  console.log(`server listening on port http://localhost:${server.address().port}`)
+findAvailablePort(port).then(port => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`)
+  })
 })
