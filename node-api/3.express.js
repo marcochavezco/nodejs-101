441690c8cc60029043a9d0e1ba6d8a745c839800
marcochavezco <machavezco@gmail.com>
2024-06-13 12:21:42 -0600

    use express middleware equivalent

--- node-api/3.express.js
+++ node-api/3.express.js
@@ -7,26 +7,31 @@ const PORT = process.env.PORT ?? 1235
 const app = express()
 app.disable('x-powered-by')
 
-app.use((req, res, next) => {
-  console.log('Mi first middleware')
-  // look for cookies
-  // track user
-  next()
-})
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   let body = ''
+
+//   req.on('data', (chunk) => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     req.body = data
+//     next()
+//   })
+// })
 
 app.get('/pokemon/ditto', (req, res) => {
   res.json(dittoJSON)
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-  req.on('data', (chunk) => {
-    body += chunk.toString()
-  })
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    res.status(201).json(data)
-  })
+  res.status(201).json(req.body)
 })
 
 app.use((req, res) => {
