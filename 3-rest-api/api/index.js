8377cc7a7768dcb2787ee7dee7eaf92d8ab3c52f
marcochavezco <machavezco@gmail.com>
2024-08-12 17:54:49 -0600

    change app.js location to api folder and rename file

--- 3-rest-api/api/index.js
+++ 3-rest-api/api/index.js
@@ -2,8 +2,8 @@ const express = require('express') // require -> commonJS
 const crypto = require('node:crypto')
 const cors = require('cors')
 
-const movies = require('./movies.json')
-const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+const movies = require('../movies.json')
+const { validateMovie, validatePartialMovie } = require('../schemas/movies')
 
 const app = express()
 app.use(express.json())
