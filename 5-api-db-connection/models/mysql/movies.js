88e9e838cc0c7ec33bd245f1352f757c7b8f345d
marcochavezco <machavezco@gmail.com>
2024-10-10 20:02:25 -0600

    getAll mysql method

--- 5-api-db-connection/models/mysql/movies.js
+++ 5-api-db-connection/models/mysql/movies.js
@@ -12,7 +12,34 @@ const connection = await mysql.createConnection(config)
 
 export class MoviesModel {
   static async getAll ({ genre }) {
-    const [movies] = await connection.query('SELECT * FROM movie')
+    if (genre) {
+      const [genres] = await connection.query('SELECT id, name FROM genre WHERE name = ?', [genre.toLowerCase()])
+
+      if (genres.length === 0) return 0
+
+      const [{ id }] = genres
+
+      const [moviesGenre] = await connection.query(`
+        SELECT
+            BIN_TO_UUID(movie_genres.movie_id) AS id,
+            movie.title,
+            genre.name
+        FROM
+            movie_genres
+        JOIN
+            movie ON movie.id = movie_genres.movie_id
+        JOIN
+            genre ON movie_genres.genre_id = genre.id
+        WHERE
+            movie_genres.genre_id = ?;`, [id])
+
+      return moviesGenre
+    }
+
+    const [movies] = await connection.query(`
+      SELECT BIN_TO_UUID(id) AS id, title, year, director, duration, poster, rate FROM movie;
+      `)
+
     return movies
   }
 
